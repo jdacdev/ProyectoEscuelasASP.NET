c73b67ec932dc9ec367c2ddd22ab99db532ad287
Juan Amaya <juan.amayacardenas@outlook.es>
2020-06-03 21:59:41 -0500

    se añade context de escuela

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using HolaMundoMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,20 +8,20 @@ namespace HolaMundoMVC.Controllers
 {
     public class EscuelaController : Controller
     {
+        private EscuelaContext _context;
         public IActionResult Index()
         {
-            #region  set Escuelas
-            var escuela = new Escuela("Escuela de Juan",2020,TiposEscuela.Secundaria);
-            escuela.Ciudad = "Bogotá";
-            escuela.Pais = "Colombia";
-            escuela.Direccion = "Calle falsa 123";
-            
-            #endregion  set Escuelas
-
+            var escuelaTemp = _context.Escuelas.FirstOrDefault();
             ViewBag.FechaActual = System.DateTime.Today;
+
             // Por convension ...
             // si no se le especifica una vista el metodo retornara la vista que corresponda con su nombre en las views de Escuela
-            return View(escuela);
+            return View(escuelaTemp);
+        }
+
+        public EscuelaController(EscuelaContext context)
+        {
+            _context = context;
         }
     }
 }
---
+++ Models/EscuelaContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HolaMundoMVC.Models
+{
+    public class EscuelaContext: DbContext
+    {
+        public DbSet<Escuela> Escuelas {get; set;}
+
+        public DbSet<Asignatura> Asignaturas {get; set;}
+
+        public DbSet<Alumno> Alumnos {get; set;}
+
+        public DbSet<Curso> Cursos {get; set;}
+
+        public DbSet<Evaluacion> Evaluaciones {get; set;}
+
+        public EscuelaContext(DbContextOptions<EscuelaContext> options): base(options)
+        {
+            
+        }
+    }
+}
