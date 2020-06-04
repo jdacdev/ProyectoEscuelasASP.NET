f801a37a1b74f4b77dbb02bad0aeeb773b9548f5
Juan Amaya <juan.amayacardenas@outlook.es>
2020-06-03 22:31:11 -0500

    Siembra de datos

--- Models/EscuelaContext.cs
+++ Models/EscuelaContext.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace HolaMundoMVC.Models
@@ -17,6 +19,56 @@ namespace HolaMundoMVC.Models
         public EscuelaContext(DbContextOptions<EscuelaContext> options): base(options)
         {
             
+        }
+        ///
+        ///Metodo que se ejeucta cuando se esta creando la BD
+        ///Se realiza Siembra de datos
+        ///
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //ejecute la instruccion original de OnModelCreating
+            base.OnModelCreating(modelBuilder);
+
+            #region Objeto escuela
+            var escuela = new Escuela("San Juan", 1995);
+            escuela.Ciudad = "Bogóta";
+            escuela.Pais = "Colombia";
+            escuela.Direccion = "Calle false 123";
+            escuela.TipoEscuela = TiposEscuela.Secundaria;
+            #endregion Objeto escuela
+            //en caso de que escuela no tenga datos insertar el objeto escuela (para efectos de demostracion)
+            modelBuilder.Entity<Escuela>().HasData(escuela);
+            //... lo mismo con las asignaturas.
+            modelBuilder.Entity<Asignatura>().HasData(
+                                                        new Asignatura(){Nombre = "Matematicas"},
+                                                        new Asignatura(){Nombre = "Sociales"},
+                                                        new Asignatura(){Nombre = "Naturales"},
+                                                        new Asignatura(){Nombre = "Ingles"},
+                                                        new Asignatura(){Nombre = "Español"}
+                                                    );
+            //... y con Alumnos
+            //ojo HasData recibe solo objetos y arrays, no colecciones de otro tipo
+            modelBuilder.Entity<Alumno>().HasData(AlumnosAlAzar().ToArray());                                   
+
+        }
+
+        public List<Alumno> AlumnosAlAzar()
+        {
+            //Generamos alumnos al azar a partir del algoritmo implementado en el curso anterior
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+
+            //Producto cartesiano con LinQ
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+
+            List<Alumno> listaAlumnosFiltrada = listaAlumnos.OrderBy((al) => al.Identificador).Take(10).ToList();
+
+            return listaAlumnosFiltrada;
+            //return View(listaAlumnosFiltrada);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HolaMundoMVC.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +15,28 @@ namespace HolaMundoMVC
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            var host = CreateHostBuilder(args).Build();
+
+            // por medio del host accedemos al context antes de poner arriba el servicio
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<EscuelaContext>();
+
+                    context.Database.EnsureCreated();//asegurese de que la base de datos esta creada
+                    //... con esto nos aseguramos de que el OnCreatingModel se ejecuto y con ello la siembra de nuestros datos.
+                }catch(Exception exc)
+                {
+                    //escribimos en el Log
+                    var logger = services.GetRequiredService<ILogger>();
+                    logger.LogError(exc, "ocurrio un error conectando con la BD.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
