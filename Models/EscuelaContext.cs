0d7f196ade526772b09d5aa1f968281f30a36a91
Juan Amaya <juan.amayacardenas@outlook.es>
2020-06-05 23:02:26 -0500

    Se adapta al modelo de datos la siembra segun las consideraciones de entity

--- Models/EscuelaContext.cs
+++ Models/EscuelaContext.cs
@@ -36,23 +36,75 @@ namespace HolaMundoMVC.Models
             escuela.Direccion = "Calle false 123";
             escuela.TipoEscuela = TiposEscuela.Secundaria;
             #endregion Objeto escuela
-            //en caso de que escuela no tenga datos insertar el objeto escuela (para efectos de demostracion)
-            modelBuilder.Entity<Escuela>().HasData(escuela);
-            //... lo mismo con las asignaturas.
-            modelBuilder.Entity<Asignatura>().HasData(
-                                                        new Asignatura(){Nombre = "Matematicas"},
-                                                        new Asignatura(){Nombre = "Sociales"},
-                                                        new Asignatura(){Nombre = "Naturales"},
-                                                        new Asignatura(){Nombre = "Ingles"},
-                                                        new Asignatura(){Nombre = "Español"}
-                                                    );
-            //... y con Alumnos
-            //ojo HasData recibe solo objetos y arrays, no colecciones de otro tipo
-            modelBuilder.Entity<Alumno>().HasData(AlumnosAlAzar().ToArray());                                   
 
+            //carga de cursos de la escuela
+            var cursos = CargarCursos(escuela);
+
+            //cargar asignaturas de curso
+            var asignaturas = CargarAsignaturas(cursos);
+
+            //cargar alumnos
+            var alumnos = CargarAlumnos(cursos);
+
+            //agregamos al contexto los elementos...
+
+            modelBuilder.Entity<Escuela>().HasData(escuela);
+            modelBuilder.Entity<Curso>().HasData(cursos.ToArray());
+            modelBuilder.Entity<Asignatura>().HasData(asignaturas.ToArray());
+            modelBuilder.Entity<Alumno>().HasData(alumnos.ToArray());
         }
 
-        public List<Alumno> AlumnosAlAzar()
+        private static List<Asignatura> CargarAsignaturas(List<Curso> cursos)
+        {
+            List<Asignatura> listaAsignaturas = new List<Asignatura>();
+            foreach (var curso in cursos)
+            {
+
+                curso.Asignaturas = new List<Asignatura>{
+                                                            new Asignatura() { Nombre = "Matematicas" ,CursoId = curso.Identificador},
+                                                            new Asignatura() { Nombre = "Sociales" ,CursoId = curso.Identificador},
+                                                            new Asignatura() { Nombre = "Naturales" ,CursoId = curso.Identificador},
+                                                            new Asignatura() { Nombre = "Ingles" ,CursoId = curso.Identificador},
+                                                            new Asignatura() { Nombre = "Español" ,CursoId = curso.Identificador}
+                                                            };
+
+                var tmpList = new List<Asignatura> {
+                                                        new Asignatura() { Nombre = "Matematicas" ,CursoId = curso.Identificador},
+                                                        new Asignatura() { Nombre = "Sociales" ,CursoId = curso.Identificador},
+                                                        new Asignatura() { Nombre = "Naturales" ,CursoId = curso.Identificador},
+                                                        new Asignatura() { Nombre = "Ingles" ,CursoId = curso.Identificador},
+                                                        new Asignatura() { Nombre = "Español" ,CursoId = curso.Identificador}
+                                                    };
+                listaAsignaturas.AddRange(tmpList);
+            }
+
+            return listaAsignaturas;
+        }
+
+        private static List<Curso> CargarCursos(Escuela escuela)
+        {
+            var listaCursos = new List<Curso>()
+            {
+                new Curso(){ Nombre = "Curso 101", Jornada = TiposJornada.Mañana, EscuelaId = escuela.Identificador},
+                new Curso(){ Nombre = "Curso 102", Jornada = TiposJornada.Mañana, EscuelaId = escuela.Identificador},
+                new Curso(){ Nombre = "Curso 103", Jornada = TiposJornada.Mañana, EscuelaId = escuela.Identificador}
+            };
+            return listaCursos;
+        }
+        private List<Alumno> CargarAlumnos(List<Curso> cursos)
+        {
+            var listaAlumnos = new List<Alumno>();
+
+            foreach (var curso in cursos)
+            {
+                var alumnos = AlumnosAlAzar(curso,10);
+                listaAlumnos.AddRange(alumnos);
+            }
+
+            return listaAlumnos;
+        }
+
+        public List<Alumno> AlumnosAlAzar(Curso curso,int cantidad)
         {
             //Generamos alumnos al azar a partir del algoritmo implementado en el curso anterior
             string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
@@ -63,9 +115,9 @@ namespace HolaMundoMVC.Models
             var listaAlumnos = from n1 in nombre1
                                from n2 in nombre2
                                from a1 in apellido1
-                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}" , CursoId = curso.Identificador};
 
-            List<Alumno> listaAlumnosFiltrada = listaAlumnos.OrderBy((al) => al.Identificador).Take(10).ToList();
+            List<Alumno> listaAlumnosFiltrada = listaAlumnos.OrderBy((al) => al.Identificador).Take(cantidad).ToList();
 
             return listaAlumnosFiltrada;
             //return View(listaAlumnosFiltrada);
